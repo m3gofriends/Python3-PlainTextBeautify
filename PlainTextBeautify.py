e3c326f98bf19cf76ee3692a19af8cd2706a0335
m3gofriends <m3gofriends@gmail.com>
2020-11-02 19:02:25 +0800

    Version 1.3 fix the input error

--- PlainTextBeautify.py
+++ PlainTextBeautify.py
@@ -6,7 +6,7 @@ programming_language_dic = {
     }
 
 try:
-    beautify_char = programming_language_dic.get(int(input("Input your Programming language number:\n0 : Support Python\n1 : Support C, C++, C#, Java, JavaScript\n2 : Input string by yourself\n")), -1)
+    beautify_char = programming_language_dic.get(int(input("Input your programming language number:\n0 : Support Python\n1 : Support C, C++, C#, Java, JavaScript\n2 : Input string by yourself\n")), -1)
 except:
     print("Program terminated with wrong input.")
     sys.exit(0)
@@ -18,15 +18,13 @@ if beautify_char == -1:
 
 beautify_list = []
 textfile_path = input("Input your Text file path:\n")
-print(" -----Begin -----") 
+print("-----Begin -----") 
 f = open(textfile_path, 'r+', encoding = 'utf8')
 
 for line in f:
     for position in range(len(beautify_char)):
         next_position = 0
         for times in range(line.count(beautify_char[position])):
-            a = line.count(beautify_char[position])
-            c = line.find(beautify_char[position],next_position + 1)
             if line[line.find(beautify_char[position],next_position + 1) + 1] != ' ' and line[line.find(beautify_char[position],next_position + 1) + 1] not in beautify_char:
                 line = line[:line.find(beautify_char[position],next_position + 1) + 1] + ' ' + line[line.find(beautify_char[position],next_position + 1) + 1:]
             if line[line.find(beautify_char[position],next_position + 1) - 1] != ' ' and line[line.find(beautify_char[position],next_position + 1) - 1] not in beautify_char:
@@ -38,7 +36,7 @@ for line in f:
     print(line, end = "")
     beautify_list.append(line)
 
-print("\n -----End -----") 
+print("\n-----End -----") 
 ans = input("\n-----Change? Y/N-----\n") 
 if ans.upper() == "Y" or ans.upper() == "YES":
     f.seek(0)
