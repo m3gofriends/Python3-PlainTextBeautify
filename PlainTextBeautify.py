6ba70df2e25de2c5d639c74579d450dfaf1402fa
m3gofriends <m3gofriends@gmail.com>
2020-11-04 19:25:13 +0800

    Version 1.5 improve the function of dictionary

--- PlainTextBeautify.py
+++ PlainTextBeautify.py
@@ -1,8 +1,8 @@
 import sys
 
-programming_language_dic = { #["all_beautify_char", "left_beautify_char", "right_beautify_char", "next_line_char"]
-    0 : ["+-*/%=!><&|^~,", "~", ",", None], #Support Python
-    1 : ["+-*/%=!><&|?^~,#", "~", ",", "#"], #Support C, C++, C#, Java, JavaScript
+programming_language_dic = { #["all_beautify_char", "right_beautify_char", "do_nothing_char"]
+    0 : ["+-*/%=!><&|^,", ",", None], #Support Python
+    1 : ["+-*/%=!><&|?^,", ",", "#"], #Support C, C++, C#, Java, JavaScript
     2 : []
     }
 
@@ -13,8 +13,8 @@ except:
     sys.exit(0)
 
 if beautify_char == []:
-    print("Input four string follow a sequence:\nall_beautify_char\nleft_beautify_char\nright_beautify_char\nnext_line_char")
-    for i in range(4):
+    print("Input three string follow a sequence:\nall_beautify_char\nright_beautify_char\ndo_nothing_char")
+    for i in range(3):
         beautify_char.append(input())
 if beautify_char == -1:
     sys.exit(0)
@@ -25,15 +25,17 @@ print("-----Begin -----")
 f = open(textfile_path, 'r+', encoding = 'utf8')
 
 for line in f:
-    if beautify_char[3] != None and beautify_char[3] in line:
+    if beautify_char[2] != None and beautify_char[2] in line:
         print(line, end = "")
         continue
     for position in range(len(beautify_char[0])):
         next_position = line.rfind(beautify_char[0][position])
         for times in range(line.count(beautify_char[0][position])):
-            if line[line.rfind(beautify_char[0][position], 0, next_position + 1) + 1] != ' ' and line[line.rfind(beautify_char[0][position], 0, next_position+1) + 1] not in beautify_char[0] and line[line.rfind(beautify_char[0][position], 0, next_position + 1)] not in beautify_char[1]:
+            if beautify_char[0][position] == "=" and line[line.rfind(beautify_char[0][position], 0, next_position + 1) + 1] != ' ' and line[line.rfind(beautify_char[0][position], 0, next_position + 1) + 1] != '=':  
+                line = line[:line.rfind(beautify_char[0][position], 0, next_position + 1) + 1] + ' ' + line[line.rfind(beautify_char[0][position], 0, next_position + 1) + 1:] 
+            elif line[line.rfind(beautify_char[0][position], 0, next_position + 1) + 1] != ' ' and line[line.rfind(beautify_char[0][position], 0, next_position+1) + 1] not in beautify_char[0] and line[line.rfind(beautify_char[0][position], 0, next_position + 1)] not in beautify_char[1]:
                 line = line[:line.rfind(beautify_char[0][position], 0, next_position + 1) + 1] + ' ' + line[line.rfind(beautify_char[0][position], 0, next_position + 1) + 1:]
-            if line[line.rfind(beautify_char[0][position], 0, next_position + 1) - 1] != ' ' and line[line.rfind(beautify_char[0][position], 0, next_position + 1) - 1] not in beautify_char[0] and line[line.rfind(beautify_char[0][position], 0, next_position + 1)] not in beautify_char[2]:
+            if line[line.rfind(beautify_char[0][position], 0, next_position + 1) - 1] != ' ' and line[line.rfind(beautify_char[0][position], 0, next_position + 1) - 1] not in beautify_char[0] and line[line.rfind(beautify_char[0][position], 0, next_position + 1)]:
                 line = line[:line.rfind(beautify_char[0][position], 0, next_position + 1)] + ' ' + line[line.rfind(beautify_char[0][position], 0, next_position + 1):]
             next_position = line.rfind(beautify_char[0][position], 0, next_position)
     print(line, end = "")
