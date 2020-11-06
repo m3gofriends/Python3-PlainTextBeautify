21ef4f42c0b926ecc0877e359a67f3ff0481ac7e
m3gofriends <m3gofriends@gmail.com>
2020-11-07 01:26:26 +0800

    Version 1.6 improve the function of dictionary and scanning (+1 squashed commits)
    Squashed commits:
    
    [d3e37fe] Version 1.6 improve the function of dictionary and scanning

--- PlainTextBeautify.py
+++ PlainTextBeautify.py
@@ -40,14 +40,14 @@ for line in f:
                 next_position = line.rfind(beautify_char[0][position], 0, next_position)
                 continue
             elif beautify_char[0][position] == "*" and beautify_char[2] == True and any(pointer in line for pointer in beautify_char[3]):
-                for pointer_position in range(len(beautify_char[3])): 
+                for pointer_position in range(len(beautify_char[3])):
                     if beautify_char[3][pointer_position] in line and line.index(beautify_char[3][pointer_position]) <= line.rfind(beautify_char[0][position], 0, next_position + 1) and line.index(beautify_char[3][pointer_position]) + len(beautify_char[3][pointer_position]) > line.rfind(beautify_char[0][position], 0, next_position + 1): 
                         continue         
             elif beautify_char[0][position] == "=" and line[line.rfind(beautify_char[0][position], 0, next_position + 1) + 1] not in " =":
                 line = line[:line.rfind(beautify_char[0][position], 0, next_position + 1) + 1] + ' ' + line[line.rfind(beautify_char[0][position], 0, next_position + 1) + 1:]
-            elif line[line.rfind(beautify_char[0][position], 0, next_position + 1) + 1] not in " '\"" and line[line.rfind(beautify_char[0][position], 0, next_position+1) + 1] not in beautify_char[0]:
+            elif line[line.rfind(beautify_char[0][position], 0, next_position + 1) + 1] not in " '\"=" and line[line.rfind(beautify_char[0][position], 0, next_position+1) + 1] != beautify_char[0][position]:
                 line = line[:line.rfind(beautify_char[0][position], 0, next_position + 1) + 1] + ' ' + line[line.rfind(beautify_char[0][position], 0, next_position + 1) + 1:]
-            if line[line.rfind(beautify_char[0][position], 0, next_position + 1) - 1] not in " '\"" and line[line.rfind(beautify_char[0][position], 0, next_position + 1) - 1] not in beautify_char[0] and line[line.rfind(beautify_char[0][position], 0, next_position + 1)] not in beautify_char[1]:
+            if line[line.rfind(beautify_char[0][position], 0, next_position + 1) - 1] not in " '\"=" and line[line.rfind(beautify_char[0][position], 0, next_position + 1) - 1] not in beautify_char[0] and line[line.rfind(beautify_char[0][position], 0, next_position + 1)] not in beautify_char[1]:
                 line = line[:line.rfind(beautify_char[0][position], 0, next_position + 1)] + ' ' + line[line.rfind(beautify_char[0][position], 0, next_position + 1):]
             next_position = line.rfind(beautify_char[0][position], 0, next_position)
     print(line, end = "")
